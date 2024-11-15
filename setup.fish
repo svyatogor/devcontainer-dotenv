f54a12577e9cd3329df6f0fbb35bf6d48f858234
Sergey Kuleshov <sergey@kuleshov.online>
2024-11-15 17:01:29 +0200

    wip

--- setup.fish
+++ setup.fish
@@ -11,3 +11,11 @@ set -U hydro_symbol_git_dirty " ⚬"
 set -U EZA_STANDARD_OPTIONS --group --header --group-directories-first --icons
 
 yes | fish_config theme save "Catppuccin Frappe"
+
+abbr --add --global g git
+abbr --add --global gss git status
+abbr --add --global gp git push
+
+mkdir -p ~/.config/fish/conf.d
+
+cp ~/dotfiles/node-bin-path.fish ~/.config/fish/conf.d/00-path.fish
