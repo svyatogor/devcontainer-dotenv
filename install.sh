9258880a5262a076c01d860bf254c6430f34d2e0
Sergey Kuleshov <svyatogor@gmail.com>
2024-11-15 22:39:44 +0200

    added bat

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install -y fish eza fzf bat stow
+sudo ln -s /usr/bin/batcat /usr/local/bin/bat
 
 rm -rf ~/.config/fish
 stow fish
