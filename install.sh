45cc46d41445ce42dcfbf304b62964973efc8b26
Sergey Kuleshov <sergey@kuleshov.online>
2024-11-15 16:58:15 +0200

    wip

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install -y fish eza fzf
 sudo usermod --shell /usr/bin/fish vscode
-fish ./setup.fish
+fish ~/dotfiles/setup.fish
 exit 0
