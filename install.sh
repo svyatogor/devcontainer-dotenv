416836b0b4cac3908771dae6fb3fc3e9555bf145
Sergey Kuleshov <svyatogor@gmail.com>
2024-11-15 17:28:28 +0200

    wip

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install -y fish eza fzf
-sudo usermod --shell /usr/bin/fish vscode
 
 mkdir -p ~/.config/fish/conf.d
 
 for file in ~/dotfiles/fish/conf.d/*; do
   ln -sf "$file" ~/.config/fish/conf.d/
 done
+
+echo "Setting up fish"
+~/dotfiles/setup.fish
+echo "Setting up fish done"
